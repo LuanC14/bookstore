f2749a2826e3cc708c2c4493f3dfda543c613662
Luan Chrystian <luanchr14@gmail.com>
2024-09-08 17:38:38 -0300

    chore: criando purchase controller, model e migrations

---
+++ src/main/kotlin/dev/luan/bookstore/controller/request/PostPurchaseRequest.kt
@@ -0,0 +1,17 @@
+package dev.luan.bookstore.controller.request
+
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
+
+data class PostPurchaseRequest(
+
+    @field:NotNull
+    @field:Positive
+    val customerId: Int,
+
+    @field:NotNull
+    val bookIds: Set<Int>
+
+) {
+
+}
