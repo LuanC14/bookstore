836277ca3b933695d29270c0eabde7db2ad4fafb
Luan Chrystian <luanchr14@gmail.com>
2024-09-08 17:00:23 -0300

    feat: swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+
 	implementation("org.flywaydb:flyway-database-postgresql")
 	runtimeOnly ("org.postgresql:postgresql")
 
---
+++ src/main/kotlin/dev/luan/bookstore/config/SwaggerConfig.kt
@@ -0,0 +1,17 @@
+package dev.luan.bookstore.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun customApi(): OpenAPI {
+
+        return OpenAPI()
+            .info(Info().title("Bookstore API").version("0.0.1"))
+    }
+}
